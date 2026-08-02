7ab2b87dcae94af5c381707399ed2ad14e2cc245
Montielboss <carloshmontielh@gmail.com>
2026-08-01 21:07:29 -0700

    Register service worker on window load

--- script.js
+++ script.js
@@ -606,3 +606,9 @@ function renderResults(){
 }
 
 renderIntro();
+
+if("serviceWorker" in navigator){
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("service-worker.js").catch(() => {});
+  });
+}
