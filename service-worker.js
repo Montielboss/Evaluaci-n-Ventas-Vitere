06064db2a079c3113a378ecb8f67187c193b9b98
Montielboss <carloshmontielh@gmail.com>
2026-08-03 21:35:34 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -8,7 +8,7 @@
    el teléfono puede seguir usando los archivos viejos guardados.
    ========================================================================== */
 
-const CACHE_NAME = "eval-pv-v1.2";
+const CACHE_NAME = "eval-pv-v1.3";
 
 const CORE_ASSETS = [
   "./",
