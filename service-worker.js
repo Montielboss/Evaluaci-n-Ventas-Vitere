000c9c049ea14548aaea02572bfbb5a4fb3454bb
Montielboss <carloshmontielh@gmail.com>
2026-09-01 19:39:44 -0700

    Increment CACHE_NAME version to v1.7
    Updated CACHE_NAME from 'eval-pv-v1.6' to 'eval-pv-v1.7' to reflect version change.

--- service-worker.js
+++ service-worker.js
@@ -8,7 +8,7 @@
    el teléfono puede seguir usando los archivos viejos guardados.
    ========================================================================== */
 
-const CACHE_NAME = "eval-pv-v1.6";
+const CACHE_NAME = "eval-pv-v1.7";
 
 const CORE_ASSETS = [
   "./",
