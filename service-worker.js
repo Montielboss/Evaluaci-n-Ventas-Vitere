bdfe550eb006169b807a2f43bd6c8d6ea1d8832d
Montielboss <carloshmontielh@gmail.com>
2026-09-04 18:58:15 -0700

    Change CACHE_NAME to version 2
    Updated CACHE_NAME from 'eval-pv-v1.9' to 'eval-pv-v2' to manage cache versioning.

--- service-worker.js
+++ service-worker.js
@@ -8,7 +8,7 @@
    el teléfono puede seguir usando los archivos viejos guardados.
    ========================================================================== */
 
-const CACHE_NAME = "eval-pv-v1.9";
+const CACHE_NAME = "eval-pv-v2";
 
 const CORE_ASSETS = [
   "./",
