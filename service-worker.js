4d91924b0b7c280a2d6c28bba2af2a13709a22d5
Montielboss <carloshmontielh@gmail.com>
2026-09-12 14:01:05 -0700

    Increment CACHE_NAME version to 2.4.1
    Updated CACHE_NAME to version 2.4.1

--- service-worker.js
+++ service-worker.js
@@ -8,7 +8,7 @@
    el teléfono puede seguir usando los archivos viejos guardados.
    ========================================================================== */
 
-const CACHE_NAME = "eval-pv-v2.4";
+const CACHE_NAME = "eval-pv-v2.4.1";
 
 const CORE_ASSETS = [
   "./",
